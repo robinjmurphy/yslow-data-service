899122e851e3ded8cd4c7e7bc693e012597011be
Robin Murphy <robin@robinmurphy.co.uk>
2014-01-04 22:55:04 +0000

    Change variable names in db helper

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
-var monk = require('monk');
-var config = require('./config.json');
-var dbPath = process.env.YSLOW_DATA_DB || config.defaults.db;
-var db = monk(dbPath);
+var config = require('./config');
+var connectionString = process.env.YSLOW_DATA_DB || config.defaults.db;
+var db = require('monk')(connectionString);
 var chalk = require('chalk');
 
-console.info('Using database:', chalk.yellow(dbPath));
+console.info('Using database:', chalk.yellow(connectionString));
 
 module.exports = {
   results: db.get('results')
